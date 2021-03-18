ed6dd56d0a46b0178e0d11302b0f93d30e4a4e3e
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-18 17:15:13 -0500

    updating readme

--- examples/Pca9685-Servo-Led-Lib-example/Pca9685-Servo-Led-Lib-example.ino
+++ examples/Pca9685-Servo-Led-Lib-example/Pca9685-Servo-Led-Lib-example.ino
@@ -4,9 +4,15 @@
    Support adarshkumarsingh83@gmail.com
 */
 
+/*
+   Adarsh Model Trains
+   Developed by Adarsh kumar
+   Support adarshkumarsingh83@gmail.com
+*/
+
 #include "Pca9685Board.h"
-#define NO_OF_SERVO_BOARDS 1
-#define NO_OF_LIGHT_BOARDS 1
+#define NO_OF_SERVO_BOARDS 2
+#define NO_OF_LIGHT_BOARDS 2
 
 int MAX_PINS = 0;
 
@@ -23,17 +29,17 @@ void setup() {
 
 void loop() {
 
-  for (int i = 1; i <= 16; i++) {
+  for (int i = 1; i <= 32; i++) {
     int pinNo = i;
     pca9685Board.throwSwitch(pinNo);
-    pca9685Board.switchOn(pinNo + 16);
+    pca9685Board.switchOn(pinNo + 32);
   }
   pca9685Board.displayPinState();
   delay(1000);
-  for (int i = 1; i <= 16; i++) {
+  for (int i = 1; i <= 32; i++) {
     int pinNo = i;
     pca9685Board.closeSwitch(pinNo);
-    pca9685Board.switchOff(pinNo + 16);
+    pca9685Board.switchOff(pinNo + 32);
   }
   pca9685Board.displayPinState();
   delay(1000);
